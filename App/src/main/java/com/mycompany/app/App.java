559f42d0bc9c2862cb922c67bca1626e40c49d84
Lina-Valderrama <lsvalderrama@uniempresarial.edu.co>
2026-09-01 11:00:54 -0500

    Crear la clase madre o superclase Viaje

---
+++ App/src/main/java/com/mycompany/app/App.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.app;
+
+/**
+ *
+ * @author uestudiantes
+ */
+public class App {
+
+    public static void main(String[] args) {
+        
+    }
+}
---
+++ Car/src/main/java/com/mycompany/car/Car.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.car;
+
+/**
+ *
+ * @author uestudiantes
+ */
+public class Car {
+
+    public static void main(String[] args) {
+        int modelo;
+        boolean encender;
+        String ;
+        
+    }
+}
